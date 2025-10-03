dec35cf71911985c42f77387288c6029c3a11687
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-03 23:44:49 +0200

    ddddd

--- graph.py
+++ graph.py
@@ -1,14 +1,19 @@
+import os
+
 from neo4j import GraphDatabase, Driver, basic_auth
 from lib import edges
 from lib import vertices
 from lib import cypher
 
-URI = "neo4j+s://64de9e83.databases.neo4j.io"
-USERNAME = "neo4j"
-PASSWORD = "3oWWBg2lrkWrdQVi9RHQpDd2C51SqYl9RT04LDrngpA"
-
 if __name__ == '__main__':
-    driver: Driver = GraphDatabase.driver(URI, auth=basic_auth(USERNAME, PASSWORD))
+    db_username = os.getenv("NEO4J_USERNAME")  
+    db_password = os.getenv("NEO4J_PASSWORD")
+    db_uri = os.getenv("NEO4J_URI")
+
+    driver: Driver = GraphDatabase.driver(
+        db_uri, 
+        auth=basic_auth(db_username, db_password)
+    )
     with driver.session() as session:
         for city in vertices.cities():
             session.execute_write(cypher.create_city, city)
