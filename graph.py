81f8d5cac8f2f72919ba34f2538f2ddabaafb6b2
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-03 23:48:09 +0200

    dddd

--- graph.py
+++ graph.py
@@ -9,12 +9,13 @@ if __name__ == '__main__':
     db_username = os.getenv("NEO4J_USERNAME")  
     db_password = os.getenv("NEO4J_PASSWORD")
     db_uri = os.getenv("NEO4J_URI")
+    db_database = os.getenv("NEO4J_DATABASE")
 
     driver: Driver = GraphDatabase.driver(
         db_uri, 
         auth=basic_auth(db_username, db_password)
     )
-    with driver.session() as session:
+    with driver.session(database=db_database) as session:
         for city in vertices.cities():
             session.execute_write(cypher.create_city, city)
         for city1, city2, distance, max_speed in edges.roads():
---
+++ ops/start.sh
@@ -0,0 +1,3 @@
+cd ..
+source etc/graph.env
+python3 graph.py
