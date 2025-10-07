142b85d550b588cb4a13e1a29296ff0104caa6b9
Diederick de Buck <diederickdebuck@liberator.home>
2025-10-07 17:01:48 +0200

    ddd

--- recommendation.py
+++ recommendation.py
@@ -41,7 +41,9 @@ if __name__ == "__main__":
         session.execute_write(cypher.create_likes, edges.create_likes())
         session.execute_write(cypher.create_movie_genres, edges.create_movie_genres())
         recs: List[Dict[str, str | float | int]] = session.execute_read(cypher.recommend_movies_weighted, arguments.user)
+        print(f"[{arguments.user}]")
         for rec in recs:
             print(f"{rec['recommendation']} score={rec['score']:.2f}, likes={rec['commonLikes']}")
-
+        print("*************************")
+        
     driver.close()
